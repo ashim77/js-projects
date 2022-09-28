ba27f507c7b9af966c639eeeb28f383bb75b30c0
asim59 <simt.ashim@gmail.com>
2022-09-28 20:18:53 +0600

    Counter Project
    increase, decrease and reset

---
+++ counter/app.js
@@ -0,0 +1,33 @@
+// set initial count
+let count = 0;
+
+// Select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+// console.log(btns);
+
+btns.forEach(function(btn){
+  // console.log(btn);
+  btn.addEventListener('click', function(e){
+    const styles = e.currentTarget.classList;
+    if(styles.contains('decrease')){
+      count--;
+    }
+    else if(styles.contains('increase')){
+      count++;
+    }else{
+      count = 0;
+    }
+    if(count > 0){
+      value.style.color = 'green';
+    }
+    if(count == 0){
+      value.style.color = '#000';
+    }
+    if(count < 0){
+      value.style.color = 'red';
+    }
+    value.textContent = count;
+  });
+})
