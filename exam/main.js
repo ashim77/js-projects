96219aa52975edabf82db01554c1b871f7843e3e
asim59 <simt.ashim@gmail.com>
2022-10-22 20:23:05 +0600

    questions

---
+++ exam/main.js
@@ -0,0 +1,4 @@
+const employeeList = "as";
+employeeList = "Now I am storing data!";
+console.log(employeeList);
+console.log("ashim");
---
+++ questions/app.js
@@ -0,0 +1,48 @@
+//using selectors inside the element
+// traversing the dom
+
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach(function (btn) {
+//   // console.log(btn);
+//   btn.addEventListener("click", function (e) {
+//     const qustion = e.currentTarget.parentElement.parentElement;
+//     qustion.classList.toggle("show-text");
+//     console.log(qustion);
+//   });
+// });
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  console.log(question);
+});
+
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach(function (btn) {
+//   btn.addEventListener("click", function (e) {
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle("show-text");
+//   });
+// });
+
+// const questions = document.querySelectorAll(".question");
+
+// questions.forEach(function (queston) {
+//   const btn = queston.querySelector(".question-btn");
+//   // console.log(btn);
+//   btn.addEventListener("click", function () {
+//     //remove class
+//     questions.forEach(function (item) {
+//       // console.log(queston);
+//       // console.log(item);
+//       if (item !== queston) {
+//         item.classList.remove("show-text");
+//         // console.log("ashim");
+//       }
+//     });
+
+//     queston.classList.toggle("show-text");
+//   });
+// });
